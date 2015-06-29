a8bb47948902cfd932ba2eae0013d34407949f42
aQuigs <aQuigs@users.noreply.github.com>
2015-06-28 22:36:53 -0700

    Hat counter

--- MHCScoreKeeper/src/com/sqftware/mhc/MainActivity.java
+++ MHCScoreKeeper/src/com/sqftware/mhc/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends Activity
         setContentView(R.layout.activity_main);
 
         scroller = (ScrollView) findViewById(R.id.svScroller);
-        final TextView total = (TextView) findViewById(R.id.tvTotalScore);
+        final TextView tvTotalScore = (TextView) findViewById(R.id.tvTotalScore);
         scores = new ScoreView[5];
         scores[0] = (ScoreView) findViewById(R.id.sv1);
         scores[1] = (ScoreView) findViewById(R.id.sv2);
@@ -28,8 +28,9 @@ public class MainActivity extends Activity
         scores[3] = (ScoreView) findViewById(R.id.sv4);
         scores[4] = (ScoreView) findViewById(R.id.sv5);
         for (ScoreView sv : scores)
-            sv.setExternalViews(total);
+            sv.setExternalViews(tvTotalScore);
 
+        final TextView tvHatScore = (TextView) findViewById(R.id.tvHatScore);
         final TextView prev = (TextView) findViewById(R.id.tvPrevScore);
         ((Button) findViewById(R.id.btNewGame)).setOnClickListener(new OnClickListener()
         {
@@ -37,10 +38,35 @@ public class MainActivity extends Activity
             public void onClick(View v)
             {
                 scroller.fullScroll(ScrollView.FOCUS_UP);
-                prev.setText(total.getText());
-                total.setText("0");
+                prev.setText(tvTotalScore.getText());
+                tvTotalScore.setText("0");
                 for (ScoreView sv : scores)
                     sv.clear();
+                tvHatScore.setText("0");
+            }
+        });
+
+        ((Button) findViewById(R.id.btHatsIncr)).setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                int hatScore = ScoreView.extractScore(tvHatScore) - 1;
+                tvHatScore.setText(Integer.toString(hatScore));
+                int totalScore = ScoreView.extractScore(tvTotalScore);
+                tvTotalScore.setText(Integer.toString(totalScore - 1));
+            }
+        });
+
+        ((Button) findViewById(R.id.btClearHats)).setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                int hatScore = ScoreView.extractScore(tvHatScore);
+                int totalScore = ScoreView.extractScore(tvTotalScore);
+                tvHatScore.setText("0");
+                tvTotalScore.setText(Integer.toString(totalScore - hatScore));
             }
         });
     }
